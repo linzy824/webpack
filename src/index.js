85df9ad007bf6188dbfb2de5bf9ca1d246764668
gcl <657718214@qq.com>
2019-12-06 15:28:49 +0800

    静态资源和基本流程

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.png";
+import Data from "./data.xml";
+import data from "./data.json";
+function component() {
+  var element = document.createElement("div");
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  console.log('data',Data);
+  console.log("json", data);
+  return element;
+}
+// const fun = {
+//   a: a=> {
+//     console.log(a);
+//   }
+// };
+// fun.a(10);
+document.body.appendChild(component());
